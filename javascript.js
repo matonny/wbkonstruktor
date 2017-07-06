d25654808d4ae7d158d6454675050186778de5af
matonny <onny.mat@gmail.com>
2017-07-06 13:48:05 +0200

    beta complete version

--- javascript.js
+++ javascript.js
@@ -34,3 +34,15 @@ $openMenuButton.click(function () {
 });
 
 $navigationLinks.click(closeMenu);
+
+function initMap() {
+        var uluru = {lat: 54.405455, lng: 18.642956};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 16,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
